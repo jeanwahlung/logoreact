c06ef917c7cddc68c1800f97b458191e10ab976c
jeanwahlung <jeancarloswahlung@gmail.com>
2019-02-08 09:22:40 -0600

    Updates

---
+++ precache-manifest.e31f9667800dd0b40efd1ced7bfe6de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/logoreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0f202d3d2571be2f0969",
+    "url": "/logoreact/static/js/runtime~main.0f202d3d.js"
+  },
+  {
+    "revision": "80784407a86ffc922260",
+    "url": "/logoreact/static/js/main.80784407.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/logoreact/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "80784407a86ffc922260",
+    "url": "/logoreact/static/css/main.4550324a.chunk.css"
+  },
+  {
+    "revision": "39f488036f9b34cd6a1e5c545f9e80d7",
+    "url": "/logoreact/index.html"
+  }
+];
